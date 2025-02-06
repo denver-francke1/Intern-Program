779bea76e1c5d742ee60743989a456634019b68b
Denver-Francke <denverfrancke29@gmail.com>
2025-02-06 12:12:01 +0530

    Initial commit

---
+++ Leaves2/src/Shape.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.util.Random;
+
+abstract class Shape {
+
+
+    // Abstract method to draw the shape
+    public void draw(Graphics g, int x, int y, int size) {
+
+    }
+}
